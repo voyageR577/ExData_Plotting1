f77e5cfc7db58ac4041a7648dc0eb0649ecd17a0
voyageR577 <127002031+voyageR577@users.noreply.github.com>
2023-07-02 15:54:35 +0800

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,52 @@
+# STEP 1 read the data
+
+# download the dataset if needed
+if (!file.exists("household_power_consumption.txt")) {
+    url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(url, destfile = "exdata_data_household_power_consumption.zip", method = "curl")
+    unzip("exdata_data_household_power_consumption.zip")
+    message("makes sure that the data file is in your working directory while running the script")
+}
+# install pacakges needed if not yet
+packages <- c('data.table','tidyverse')
+install.packages(setdiff(packages, rownames(installed.packages())))
+
+# load pacakges needed for this script
+library(data.table)
+library(tidyverse)
+
+# read the data into R and clean it
+file <- "household_power_consumption.txt"
+DT<-fread(file)
+DT <- DT %>%
+    filter(Date == "1/2/2007" | Date == "2/2/2007") %>%
+    as_tibble
+
+DT <- DT %>%
+    unite(Date_time,Date,Time, sep = " ")
+
+DT$Date_time <- strptime(DT$Date_time, format = "%d/%m/%Y %H:%M:%S")
+
+# change the class of column 2 to 8 to numeric
+DT[,2:8] <- 
+    DT %>%
+    select(2:8) %>%
+    type_convert("nnnnnnn")
+
+# STEP 2 plot
+
+par(mfrow=c(1,1))
+x <- DT$Date_time
+y <- DT$Sub_metering_1
+plot(x,y,type="n",xlab = "",ylab = "Energy sub metering")
+lines(x,DT$Sub_metering_1, col = "black")
+lines(x,DT$Sub_metering_2, col = "red")
+lines(x,DT$Sub_metering_3, col = "blue")
+legend("topright",
+       col = c("black","red","blue"),
+       lty = par("lty"),
+       legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       y.intersp = 1
+       )
+dev.copy(png,"plot3.png")
+dev.off()
